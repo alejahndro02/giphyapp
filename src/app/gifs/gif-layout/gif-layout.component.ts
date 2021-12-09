d18a92d50ce985189875f0f21e17b13f3ba10cc5
Alejahndro <alejahndro02@gmail.com>
2021-12-09 15:02:24 -0600

    se crearon y se maquetaron los componentes de busqueda, gifLayaout, resultado

---
+++ src/app/gifs/gif-layout/gif-layout.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-gif-layout',
+  templateUrl: './gif-layout.component.html',
+  styleUrls: ['./gif-layout.component.css']
+})
+export class GifLayoutComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
