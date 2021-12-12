f8439c09255389732cc325cde644b0b54e9f6ca6
Alejahndro <alejahndro02@gmail.com>
2021-12-12 08:46:36 -0600

    Atraves del decorador viewchild se le asigno la variable local txtBuscar que es de tipo ElementRef<HTMLInputElement, tambine se declaro el metodo Buscar

--- src/app/gifs/busqueda/busqueda.component.ts
+++ src/app/gifs/busqueda/busqueda.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { GifServiceService } from './../servies/gif-service.service';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-busqueda',
   templateUrl: './busqueda.component.html',
-  styleUrls: ['./busqueda.component.css']
 })
-export class BusquedaComponent implements OnInit {
+export class BusquedaComponent  {
 
-  constructor() { }
+  @ViewChild('txtBuscar') txtBuscar! : ElementRef<HTMLInputElement>
 
-  ngOnInit(): void {
-  }
+  constructor(private gifService:GifServiceService){}
 
+  buscar(){
+    const valor = this.txtBuscar.nativeElement.value
+    if(valor.trim().length === 0){
+      return;
+    }
+    this.gifService.buscarGifs(valor);
+    this.txtBuscar.nativeElement.value = '';
+ }
 }
