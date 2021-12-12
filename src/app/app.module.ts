768f474ec81a24ffc32500f32125512241a9d0fc
Alejahndro <alejahndro02@gmail.com>
2021-12-12 10:24:07 -0600

    Se importo el modulo de httpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,11 @@
-import { GifsModule } from './gifs/gifs.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+
+import { HttpClientModule } from '@angular/common/http'
+import { GifsModule } from './gifs/gifs.module';
 import { SharedModule } from './shared/shared.module';
+import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [
@@ -14,7 +15,8 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    GifsModule
+    GifsModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
